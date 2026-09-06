7e78ada3747ec6d9d52391dd0cccdf51587fe5d2
fahmirizki-zkie <fahmirizky2006@gmail.com>
2026-09-06 22:50:53 +0700

    merapikan konfigurasi app

---
+++ modul4/config/env.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBSSLMode  string
+}
+
+func Load() Config {
+	_ = godotenv.Load()
+
+	return Config{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+		DBSSLMode:  os.Getenv("DB_SSLMODE"),
+	}
+}
+
+func GetEnv(key, fallback string) string {
+	value := os.Getenv(key)
+
+	if value == "" {
+		return fallback
+	}
+
+	return value
+}
